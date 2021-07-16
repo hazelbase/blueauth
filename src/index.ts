e03108dd9afc1c8558932cfa2269a1608d2a0947
Adrian Artiles <adrian.r.artiles@gmail.com>
2021-07-16 15:12:39 -0700

    docs: removing commented out test code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
 export * as express from './platform/express';
 export * as nextjs from './platform/nextjs';
-
-// const greeter = (color: string) => {
-//   const welcomeSentance = `You like the color ${color}!!!!`;
-//   return welcomeSentance;
-// };
-//
-// const ageGreet = (age: number) => `You are ${age} years old`;
-//
-// export { greeter, ageGreet };
